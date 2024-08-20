056e3068499c965ff0e260e2847967a9de27feac
MilkeeyCat <milkeeycat@gmail.com>
2024-08-20 21:47:18 +0300

    fix(int_repr): add ranges for i/u 32/64 integers

--- src/parser/expr/int_repr.rs
+++ src/parser/expr/int_repr.rs
@@ -5,11 +5,19 @@ const I8_MIN: i64 = i8::MIN as i64;
 const I8_MAX: i64 = i8::MAX as i64;
 const I16_MIN: i64 = i16::MIN as i64;
 const I16_MAX: i64 = i16::MAX as i64;
+const I32_MIN: i64 = i32::MIN as i64;
+const I32_MAX: i64 = i32::MAX as i64;
+const I64_MIN: i64 = i64::MIN as i64;
+const I64_MAX: i64 = i64::MAX as i64;
 
 const U8_MIN: u64 = u8::MIN as u64;
 const U8_MAX: u64 = u8::MAX as u64;
 const U16_MIN: u64 = u16::MIN as u64;
 const U16_MAX: u64 = u16::MAX as u64;
+const U32_MIN: u64 = u32::MIN as u64;
+const U32_MAX: u64 = u32::MAX as u64;
+const U64_MIN: u64 = u64::MIN as u64;
+const U64_MAX: u64 = u64::MAX as u64;
 
 #[derive(Debug, Clone, PartialEq, Default)]
 pub struct IntLitRepr {
@@ -25,7 +33,8 @@ impl IntLitRepr {
         match self.inner {
             I8_MIN..=I8_MAX => Type::I8,
             I16_MIN..=I16_MAX => Type::I16,
-            _ => unreachable!(),
+            I32_MIN..=I32_MAX => Type::I32,
+            I64_MIN..=I64_MAX => Type::I64,
         }
     }
 
@@ -68,7 +77,8 @@ impl UIntLitRepr {
         match self.inner {
             U8_MIN..=U8_MAX => Type::U8,
             U16_MIN..=U16_MAX => Type::U16,
-            _ => unreachable!(),
+            U32_MIN..=U32_MAX => Type::U32,
+            U64_MIN..=U64_MAX => Type::U64,
         }
     }
 
