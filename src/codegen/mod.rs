f59608c86bf6734dbb3fc72237507a03a2145585
MilkeeyCat <milkeeycat@gmail.com>
2026-08-17 21:02:44 +0300

    refactor: comment out codegen part I want to continue with the compiler but `tja` crate isn't ready.

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,4 +1,6 @@
-mod function;
-mod module;
+// mod function;
+// mod module;
 
-pub use module::compile;
+//pub use module::compile;
+
+pub fn compile<'ir>(_ctx: &crate::Context<'ir>, _package: &crate::ir::Package<'ir>) {}
